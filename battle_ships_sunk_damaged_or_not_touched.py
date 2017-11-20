beb1ea5a069d5ce6f394f5251a691388b8c5f643
Telonor (Anton Viievskyi) <viievskyi.anton@gmail.com>
2017-11-20 16:37:36 +0200

    Add unfinished "todo" kata: "Battle ships: Sunk damaged or not touched?".

---
+++ battle_ships_sunk_damaged_or_not_touched.py
@@ -0,0 +1,9 @@
+"""
+Solution for "Battle ships: Sunk damaged or not touched?" kata:
+https://www.codewars.com/kata/58d06bfbc43d20767e000074/train/python
+"""
+
+
+def damaged_or_sunk (board, attacks):
+    pass
+    # Code here
